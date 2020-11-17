0a82c8ea4912cd7baf6efc298f3c3596a504e8d7
Tiko <65791040+TikoA@users.noreply.github.com>
2020-11-17 16:28:43 +0400

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import { Box, Button } from '@chakra-ui/react'
+import { useState } from 'react';
+import Palette from './components/Palettegen'
+
+function App() {
+  const [index, setIndex] = useState(0)
+
+  function handleKeyPress() {
+    setIndex(Math.floor(Math.random() * 199))
+  }
+
+  return (
+    <div className="App">
+      <Box>
+        <Box 
+          width="100%" 
+          display="flex" 
+          justifyContent="center" 
+          alignItems="center" 
+          height="20vh"
+          backgroundColor="gray.700"
+        >
+          <Button colorScheme="blue" onClick={handleKeyPress}>Generate new</Button>
+        </Box>
+        <Palette index={index}/>
+      </Box>
+    </div>
+  );
+}
+
+export default App;
